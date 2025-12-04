cf47ab9b6155a13c0ea9d483078985d5d3a6b5ef
DemonSlayer256 <143187253+DemonSlayer256@users.noreply.github.com>
2025-12-04 17:53:06 +0000

    Updated the viewsets logic and access control

--- backend/peerpals/api/urls.py
+++ backend/peerpals/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet, MentorViewSet, FeedbackViewSet, SessionViewSet
+from .views import RegisterViewSet, StudentViewSet, MentorViewSet, FeedbackViewSet, SessionViewSet, LoginAPIView
 
 router = DefaultRouter()
+router.register(r'register', RegisterViewSet)
 router.register(r'students', StudentViewSet)
 router.register(r'mentors', MentorViewSet)
 router.register(r'feedbacks', FeedbackViewSet)
@@ -10,4 +11,6 @@ router.register(r'sessions', SessionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Add login URL
+    path('login/', LoginAPIView.as_view(), name='login'),
 ]
